06f19fb1256860afd3e2a467c009e682deb5f370
weronika <weronikakoterba31@wp.pl>
2023-06-03 22:17:14 +0200

    wczytanie i naryswanie planszy

--- gipf/main.cpp
+++ gipf/main.cpp
@@ -8,34 +8,54 @@
 typedef std::vector<char> VS;
 typedef std::vector<VS> VSS;
 typedef std::unordered_map<std::string, std::pair<int, int>> UMAP_PAIR;
-using namespace std;
+typedef std::vector<std::pair<int, int>> CORR;
 
 struct game_input {
 	int S = 0;
 	int K = 0;
 	int GW = 0;
 	int GB = 0;
-	int white_pawn = 0;
-	int black_pawn = 0;
-	char active_player = '0';
+	int whitePawn = 0;
+	int blackPawn = 0;
+	char activePlayer = '0';
 };
 
 struct commands {
-	string load_board = "LOAD_GAME_BOARD";
-	string print_board = "PRINT_GAME_BOARD";
-	string do_move = "DO_MOVE";
+	std::string loadBoard = "LOAD_GAME_BOARD";
+	std::string printBoard = "PRINT_GAME_BOARD";
+	std::string doMove = "DO_MOVE";
 };
 
-void load_game_board(VSS& board, int K, int S, int& white_on_board,
-	int& black_on_board, int& valid_sign, bool& correct_board,
-	UMAP_PAIR& umap,
-	std::vector < std::pair<int, int>>& corrdinate) {
+void addToBorder(VSS& board, CORR& corrdinate){
+	for (int i = 0; i < board.size(); ++i) {
+		for (int j = 0; j < board[i].size(); j++) {
+			if (i == 1 && (int)board[i][j] > 32) {
+				corrdinate.push_back({ i, j });
+			}
+			else if (i == board.size() - 1 && (int)board[i][j] > 32) {
+				corrdinate.push_back({ i, j });
+			}
+			else if ((int)board[i][j] > 32) {
+				corrdinate.push_back({ i, j });
+				break;
+			}
+		}
+		for (int j = board[i].size() - 1; j >= 0; j--) {
+			if ((int)board[i][j] > 32) {
+				corrdinate.push_back({ i, j });
+				break;
+			}
+		}
+	}
+}
+
+void loadBoardGame(VSS& board, int K, int S, int& whiteOnBoard,int& blackOnBoard, int& validSign, bool& correctBoard,UMAP_PAIR& umap,CORR& corrdinate) {
 	int n = 1;
 	int size = 2 * S;
 	std::string sLine;
 	int SZ = S + 1;
 	for (int j = 0; j < size; j++) {
-		valid_sign = 0;
+		validSign = 0;
 		std::getline(std::cin, sLine);
 		if (sLine == "")
 			continue;
@@ -62,56 +82,32 @@ void load_game_board(VSS& board, int K, int S, int& white_on_board,
 				continue;
 			}
 			if (sign == 'W') {
-				white_on_board++;
+				whiteOnBoard++;
 			}
 			if (sign == 'B') {
-				black_on_board++;
+				blackOnBoard++;
 			}
 			if (sign != ' ' && sign != '\t') {
-				valid_sign++;
+				validSign++;
 			}
 			corrName = beginChar + std::to_string(beginIter);
 			beginChar += 1;
 			beginIter++;
 			umap[corrName] = { j - 1, index };
 		}
-		if (j <= S && valid_sign == S + j - 1) {
-			correct_board = true;
+		if (j <= S && validSign == S + j - 1) {
+			correctBoard = true;
 		}
-		else if (j > S && valid_sign == 2 * S - 1 - n) {
-			correct_board = true;
+		else if (j > S && validSign == 2 * S - 1 - n) {
+			correctBoard = true;
 			n++;
 		}
 		else if (j != 0) {
-			correct_board = false;
+			correctBoard = false;
 			break;
 		}
 	}
-	// get border
-	for (int i = 0; i < board.size(); ++i) {
-		for (int j = 0; j < board[i].size(); j++) {
-			if (i == 1 && (int)board[i][j] > 32) {
-				corrdinate.push_back({ i, j });
-				// std::cout << i << " " << j << " ";
-			}
-			else if (i == board.size() - 1 && (int)board[i][j] > 32) {
-				corrdinate.push_back({ i, j });
-				// std::cout << i << " " << j << " ";
-			}
-			else if ((int)board[i][j] > 32) {
-				corrdinate.push_back({ i, j });
-				// std::cout << i << " " << j << " ";
-				break;
-			}
-		}
-		for (int j = board[i].size() - 1; j >= 0; j--) {
-			if ((int)board[i][j] > 32) {
-				corrdinate.push_back({ i, j });
-				// std::cout << i << " " << j << " ";
-				break;
-			}
-		}
-	}
+	addToBorder(board, corrdinate);
 }
 
 char getOpositTurn(const char turn) {
@@ -124,14 +120,7 @@ UMAP_PAIR getBorder(VSS& board) {
 	UMAP_PAIR border;
 	return border;
 }
-//<key, value> vale = pair -> first. second
-void move(string& from, string& to, UMAP_PAIR& umap, VSS& board, std::vector<std::pair<int, int>>& corrdinate, char& active_player) {
-	/* MOVE_COMMITTED - ruch poprawny, zatwierdzono zmiany na planszy i zmieniono aktualnego gracza.
-		 BAD_MOVE_<x1>_IS_WRONG_INDEX - z³y indeks, pokazuje na pole spoza planszy.W przypadku kiedy podano wiêcej ni¿ jeden z³y indeks, drukowany jest tylko pierwszy napotkany.
-		 UNKNOWN_MOVE_DIRECTION - nie mo¿na okreœliæ kierunku ruchu.
-		 BAD_MOVE_<x1>_IS_WRONG_STARTING_FIELD - wybrano z³e, pole startowe(powinno to byæ jedno z po³o¿onych na obrze¿ach planszy)
-		 BAD_MOVE_<x2>_IS_WRONG_DESTINATION_FIELD - wybrano z³e, pole docelowe(powinno to byæ pole wolne albo zawieraj¹ce pion któregoœ z graczy)
-		 BAD_MOVE_ROW_IS_FULL - nie mo¿na wykonaæ ruchu bo wiersz jest zape³niony.*/
+void move(std::string& from, std::string& to, UMAP_PAIR& umap, VSS& board, CORR& corrdinate, char& active_player) {
 	const auto& f = umap.find(to);
 	int y = 0;
 	int x = 0;
@@ -140,49 +129,94 @@ void move(string& from, string& to, UMAP_PAIR& umap, VSS& board, std::vector<std
 	int fx = 0;
 	if (ffrom != umap.end()) {
 		fy = ffrom->second.first + 1;
-		fx = ffrom->second.second + 1;
+		fx = ffrom->second.second;
 		for (const auto& it : corrdinate) {
 			if (it.first == fy && it.second == fx) {
-				std::cout << "BAD_MOVE_<" << from << ">_IS_WRONG_STARTING_FIELD" << endl;
-				return;
+				break;
 			}
+			else {
+				continue;
+			}
+			//std::cout << "BAD_MOVE_<" << from << ">_IS_WRONG_STARTING_FIELD\n";
+			return;
 		}
 		if (f != umap.end()) {
 			y = f->second.first + 1;
 			x = f->second.second + 1;
 			if (board[y][x] == 'W' || board[y][x] == 'B') {
-				std::cout << "BAD_MOVE_ROW_IS_FULL" << endl;
+				std::cout << "BAD_MOVE_ROW_IS_FULL\n";
 				return;
 			}
 			if (board[y][x] != '_' ||
-				board[y][x] == active_player) { // TODO: czy to oznacza ze pionek nie moze byc
-				// tym samym pionkiem
-				std::cout << "BAD_MOVE_<" << to << ">_IS_WRONG_DESTINATION_FIELD" << endl;;
+				board[y][x] == active_player) {
+				std::cout << "BAD_MOVE_<" << to << ">_IS_WRONG_DESTINATION_FIELD\n";
 				return;
 			}
-		}else {
-			std::cout << "BAD_MOVE_<" << to << "_IS_WRONG_INDEX" << endl;;
+		}
+		else {
+			std::cout << "BAD_MOVE_<" << to << "_IS_WRONG_INDEX\n";;
 			return;
 		}
 	}
 	else {
-		std::cout << "BAD_MOVE_<" << from << "_IS_WRONG_INDEX" << endl;;
+		std::cout << "BAD_MOVE_<" << from << "_IS_WRONG_INDEX\n";
 		return;
 	}
-	board[y][x] = board[fy][fx - 1];
-	board[fy][fx - 1] = active_player;
+	board[y][x] = board[fy][fx];
+	board[fy][fx] = active_player;
 	std::cout << "MOVE_COMMITTED\n";
 	active_player = getOpositTurn(active_player);
 }
 
-void print_game_board(const VSS& board) {
+
+void printGameBoard(const VSS& board) {
 	for (int j = 1; j < board.size(); j++) {
 		for (int k = 0; k < board[j].size(); k++) {
-			cout << board[j][k];
+			std::cout << board[j][k];
 		}
-		cout << endl;
+		std::cout << "\n";
 	}
-	cout << endl;
+	std::cout << "\n";
+}
+
+void findDirection(std::string& where, std::string& from, std::string& to){
+	int idx = 0;
+	while (idx < (int)where.size() && where[idx] != '-') {
+		from += where[idx];
+		idx++;
+	}
+	idx = idx + 1;
+	while (idx < (int)where.size()) {
+		to += where[idx];
+		idx++;
+	}
+}
+
+void showResultsOfLoading(int white_on_board, game_input& input, bool& correct_board, int black_on_board){
+	if (white_on_board >(input.GW - input.whitePawn)) {
+		std::cout << "WRONG_WHITE_PAWNS_NUMBER\n";
+		correct_board = false;
+	}
+	else if (black_on_board > (input.GB - input.blackPawn)) {
+		std::cout << "WRONG_BLACK_PAWNS_NUMBER\n";
+		correct_board = false;
+	}
+	else if (correct_board == false) {
+		std::cout << "WRONG_BOARD_ROW_LENGTH\n";
+	}
+	else
+		std::cout << "BOARD_STATE_OK\n";
+}
+
+void showResultsOfPrinting(bool correct_board, game_input& input, VSS& board){
+	if (correct_board == false)
+		std::cout << "EMPTY_BOARD\n";
+	else {
+		std::cout << input.K << " " << input.S << " " << input.GW << " " << input.GB << "\n";
+		std::cout << input.whitePawn << " " << input.blackPawn << " "
+			<< input.activePlayer << "\n";
+		printGameBoard(board);
+	}
 }
 
 int main() {
@@ -190,69 +224,37 @@ int main() {
 	bool correct_board = true;
 	game_input input;
 	commands commands;
-	char turn = 0;
 	UMAP_PAIR position;
-	std::vector<pair<int, int>> corrdinate;
-	while (!cin.eof()) {
+	CORR corrdinate;
+	while (!std::cin.eof()) {
 		int white_on_board = 0;
 		int black_on_board = 0;
 		int valid_char = 0;
-		string command;
-		cin >> command;
-		if (command.compare(commands.load_board) == 0) {
+		std::string command;
+		std::cin >> command;
+		if (command.compare(commands.loadBoard) == 0) {
 			position.clear();
 			corrdinate.clear();
 			correct_board = true;
-			cin >> input.S >> input.K >> input.GW >> input.GB >> input.white_pawn >>
-				input.black_pawn >> input.active_player;
+			std::cin >> input.S >> input.K >> input.GW >> input.GB >> input.whitePawn >>
+				input.blackPawn >> input.activePlayer;
 			int line = 0;
-			int current_line = 0;
 			int size = 2 * input.S;
 			board = VSS(size);
-			load_game_board(board, input.K, input.S, white_on_board, black_on_board,
+			loadBoardGame(board, input.K, input.S, white_on_board, black_on_board,
 				valid_char, correct_board, position, corrdinate);
-			if (white_on_board > (input.GW - input.white_pawn)) {
-				cout << "WRONG_WHITE_PAWNS_NUMBER" << endl;
-				correct_board = false;
-			}
-			else if (black_on_board > (input.GB - input.black_pawn)) {
-				cout << "WRONG_BLACK_PAWNS_NUMBER" << endl;
-				correct_board = false;
-			}
-			else if (correct_board == false) {
-				cout << "WRONG_BOARD_ROW_LENGTH" << endl;
-			}
-			else
-				cout << "BOARD_STATE_OK" << endl;
+			showResultsOfLoading(white_on_board, input, correct_board, black_on_board);
 		}
-		if (command.compare(commands.print_board) == 0) {
-			if (correct_board == false)
-				cout << "EMPTY_BOARD" << endl;
-			else {
-				cout << input.K << " " << input.S << " " << input.GW << " " << input.GB
-					<< endl;
-				cout << input.white_pawn << " " << input.black_pawn << " "
-					<< input.active_player << endl;
-				print_game_board(board);
-			}
+		if (command.compare(commands.printBoard) == 0) {
+			showResultsOfPrinting(correct_board, input, board);
 		}
-		if (command.compare(commands.do_move) == 0) {
+		if (command.compare(commands.doMove) == 0) {
 			std::string where;
-			cin >> where;
+			std::cin >> where;
 			std::string from = "";
-			int idx = 0;
-			while (idx < (int)where.size() && where[idx] != '-') {
-				from += where[idx];
-				idx++;
-			}
-			idx = idx + 1;
 			std::string to = "";
-			while (idx < (int)where.size()) {
-				to += where[idx];
-				idx++;
-			}
-			move(from, to, position, board, corrdinate, input.active_player);
-			// std::cout << "[DEUBG] " << from << " " << to << std::endl;
+			findDirection(where, from, to);
+			move(from, to, position, board, corrdinate, input.activePlayer);
 		}
 	}
 }
